06f37e2e166e51822641796db8a723453c5f59d0
shivanvitha-2005 <73379879+shivanvitha-2005@users.noreply.github.com>
2020-10-28 00:09:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-AOS.init();
+// AOS.init();
+AOS.init({
+  disable: 'mobile'
+});
+
+AOS.init({
+  disable: function () {
+    var maxWidth = 800;
+    return window.innerWidth < maxWidth;
+  }
+});
