288efc584be8dccda1e5346e4e96aed580cab9cb
shivanvitha-2005 <73379879+shivanvitha-2005@users.noreply.github.com>
2020-10-28 07:26:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-// AOS.init();
+var wel = new Audio("welcome.mp3");
+wel.play();
+window.onscroll = function() {
+  myFunction()
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+  if(scrolled%2!=0)
+    document.querySelector(".tit h1").innerHTML="Get All Your Ration At Your Door Step";
+  else
+    document.querySelector(".tit h1").innerHTML="Download Your App Now";
+}
+
+
+
 AOS.init({
   disable: 'mobile'
 });
